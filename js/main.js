799a9d322b6bbc65312d3576a1adf1eaed607ecf
Prithav Jha <186852980+PrithavDevelops@users.noreply.github.com>
2026-01-18 23:15:28 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 const toggle = document.getElementById("themeToggle");
 
-// Function to close the popup
-function closePopup() {
-  document.getElementById('work-in-progress-popup').style.display = 'none';
-}
-
-// Show the popup automatically when the page loads
-window.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('work-in-progress-popup').style.display = 'flex';
-});
-
 toggle?.addEventListener("click", () => {
   document.body.classList.toggle("light");
   toggle.textContent =
